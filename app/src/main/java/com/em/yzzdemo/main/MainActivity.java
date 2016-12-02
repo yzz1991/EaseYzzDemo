35b55ce96e4473d88f0581be30ad9c8fc59a87ef
geri_yang <geri_yang@163.com>
2016-12-02 20:17:22 +0800

    完成聊天页面搭建

--- app/src/main/java/com/em/yzzdemo/main/MainActivity.java
+++ app/src/main/java/com/em/yzzdemo/main/MainActivity.java
@@ -3,8 +3,13 @@ package com.em.yzzdemo.main;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.em.yzzdemo.BaseActivity;
 import com.em.yzzdemo.R;
@@ -28,6 +33,7 @@ public class MainActivity extends BaseActivity {
     private ConversationFragment mConversationFragment;
     private SettingFragment mSettingFragment;
     private Fragment[] mFragment;
+    private int mCurrentPageIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,11 +50,12 @@ public class MainActivity extends BaseActivity {
     private void initView() {
         //设置toolbar值
         mToolbars = new String[]{"联系人","会话","设置"};
-        setSupportActionBar(toolbar);
         //设置选中第一个
-        getSupportActionBar().setTitle(mToolbars[0]);
+        toolbar.setTitle(mToolbars[0]);
         //toolbar字体颜色
         toolbar.setTitleTextColor(0xffffffff);
+        //声明toolbar
+        setSupportActionBar(toolbar);
         //创建联系人、会话、设置fragment
         mContactsFragmnet = new ContactsFragmnet();
         mConversationFragment = new ConversationFragment();
@@ -69,7 +76,15 @@ public class MainActivity extends BaseActivity {
 
             @Override
             public void onPageSelected(int position) {
+                mCurrentPageIndex = position;
                 toolbar.setTitle(mToolbars[position]);
+                if(position == 0){
+                    toolbar.getMenu().clear();
+                    toolbar.inflateMenu(R.menu.menu_concacts);
+                }else if(position == 1){
+                    toolbar.getMenu().clear();
+                    toolbar.inflateMenu(R.menu.menu_chat);
+                }
             }
 
             @Override
@@ -78,5 +93,71 @@ public class MainActivity extends BaseActivity {
             }
         });
 
+    }
+
+    //设定菜单各按钮的点击动作
+    private Toolbar.OnMenuItemClickListener onMenuItemClick = new Toolbar.OnMenuItemClickListener() {
+
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.action_call:
+                    Toast.makeText(mActivity, "call", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.action_video:
+                    Toast.makeText(mActivity, "video", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.action_more:
+                    Toast.makeText(mActivity, "more", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            return true;
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (viewPager.getCurrentItem() == 0) {
+            toolbar.inflateMenu(R.menu.menu_concacts);
+            setSearchViewQueryListener();
+        } else if (viewPager.getCurrentItem() == 1) {
+            toolbar.inflateMenu(R.menu.menu_chat);
+            setSearchViewQueryListener();
+        }
+        toolbar.setOnMenuItemClickListener(onMenuItemClick);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void setSearchViewQueryListener() {
+
+        SearchView searchView;
+        if (mCurrentPageIndex == 0) {
+            searchView = (SearchView) toolbar.getMenu().findItem(R.id.action_contacts_search).getActionView();
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override public boolean onQueryTextSubmit(String query) {
+                    return true;
+                }
+
+                @Override public boolean onQueryTextChange(String newText) {
+//                    mContactsFragmnet.filter(newText);
+                    return true;
+                }
+            });
+        } else if (mCurrentPageIndex == 1) {
+            searchView = (SearchView) MenuItemCompat.getActionView(
+                    toolbar.getMenu().findItem(R.id.action_conversation_search));
+            // search conversations list
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override public boolean onQueryTextSubmit(String query) {
+//                    mConversationFragment.filter(query);
+                    return true;
+                }
+
+                @Override public boolean onQueryTextChange(String newText) {
+//                    mConversationFragment.filter(newText);
+                    return true;
+                }
+            });
+        }
     }
 }
